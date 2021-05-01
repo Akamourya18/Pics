40f232ee4d8ecf9e171b0bb81438ba6133501172
Akash Mourya <mouryakash883@gmail.com>
2021-05-01 20:28:53 +0530

    API request

---
+++ src/components/ImageList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return <img src={image.urls.regular} />;
+  });
+  return <div>{images}</div>;
+};
+
+export default ImageList;
