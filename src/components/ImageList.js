1bbb858d223ccf7fd845dbb2f8dc2c630fefea86
Akash Mourya <mouryakash883@gmail.com>
2021-05-03 22:01:22 +0530

    Adding CSS

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,11 @@
+import './ImageList.css';
 import React from 'react';
-
+import ImageCard from './ImageCard';
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return (
-      <img alt={image.desciption} key={image.id} src={image.urls.regular} />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
