cf356a219dc458152c25f01678d89e259213744f
Akash Mourya <mouryakash883@gmail.com>
2021-05-04 22:30:29 +0530

    Final Styling

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,18 +3,24 @@ import React from 'react';
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = { spans: 0 };
+
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
     this.imageRef.current.addEventListener('load', this.setSpans);
   }
+
   setSpans = () => {
     const height = this.imageRef.current.clientHeight;
+
     const spans = Math.ceil(height / 10);
+
     this.setState({ spans });
   };
+
   render() {
     const { description, urls } = this.props.image;
 
