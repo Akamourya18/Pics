29010cce75372b65a815a586495dbac942bb63ab
Akash Mourya <mouryakash883@gmail.com>
2021-05-04 20:12:10 +0530

    Final styling

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,6 +3,7 @@ import React from 'react';
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
   }
 
@@ -10,13 +11,15 @@ class ImageCard extends React.Component {
     this.imageRef.current.addEventListener('load', this.setSpans);
   }
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
   };
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
